65ca537f282847aa1ef24eb7522a584997e1c6d2
Marcos8060 <marcosgav80@gmail.com>
2022-03-17 15:28:51 +0300

    fetched all the netflix categories using axios and displayed in the ui

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react'
 import axios from './axios'
+import "./Row.css"
 
 
 const base_url = 'https://image.tmdb.org/t/p/original/'
@@ -21,11 +22,14 @@ function Row({title,fetchUrl}) {
         <h2>{title}</h2>
 
         <div className="row__posters">
-            {movies.map((movie)=>{
-                                  return <div key={movie.id} className='row_poster'>
-                       <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
-                  </div>
-            })};
+            {movies.map((movie)=>(
+             <img 
+               key={movie.id}
+               className='row__poster' 
+               src={`${base_url}${movie.poster_path}`} 
+               alt={movie.name} 
+            />    
+            ))}
         </div>
     </div>
   )
